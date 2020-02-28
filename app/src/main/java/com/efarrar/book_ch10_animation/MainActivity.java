93caeedb1ae9a30d7f955acfe0a971e8c31a5613
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-02-28 03:08:18 +0300

    Project completed
    Code as the chapter is complete.

--- app/src/main/java/com/efarrar/book_ch10_animation/MainActivity.java
+++ app/src/main/java/com/efarrar/book_ch10_animation/MainActivity.java
@@ -2,13 +2,51 @@ package com.efarrar.book_ch10_animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TwoLineListItem;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable lightsAnimation;      //provides methods for drawable animations
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imgFrame = findViewById(R.id.imgLights);              //instantiating the imageView
+        //setting the animation-list from animation.xml as the Background property of the imgFrame ImageView
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        //accessing the animationDrawable object by getting the view object
+        lightsAnimation=(AnimationDrawable) imgFrame.getBackground();
+
+        //adding the buttons
+        Button button1 = findViewById(R.id.btnStart);
+        Button button2 = findViewById(R.id.btnStop);
+
+        /* View.OnClickListener with an Anonymous Inner Type event handler
+
+         */
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //animation is started using the stop() from AnimationDrawable class
+                lightsAnimation.start();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //animation is stopped using the stop() from AnimationDrawable class
+                lightsAnimation.stop();
+                //Launching the Tween.java class
+                startActivity(new Intent(MainActivity.this, Tween.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/efarrar/book_ch10_animation/Tween.java
@@ -0,0 +1,19 @@
+package com.efarrar.book_ch10_animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Tween extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tween);
+        ImageView imgRotate = findViewById(R.id.imgTween);
+        //starting rotation animation
+        imgRotate.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation));
+    }
+}
